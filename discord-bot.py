e41ccf33aa115a5c5be8b3624967e0bcf54ba0ae
Hackysack <tk13xr37@gmail.com>
2020-04-09 07:06:42 -0400

    Update discord-bot.py

--- discord-bot.py
+++ discord-bot.py
@@ -28,10 +28,10 @@ async def on_ready():
             embed.add_field(name=f"New Submission by {submission.author}", value=f"{submission.title}\n[Link to post](https://reddit.com{submission.permalink})")
             await ctx.send(embed=embed)
             await asyncio.sleep(.1)
-@bot.event
-async def on_command_error(ctx, error):
-    if isinstance(error, commands.CommandNotFound):
-        pass
+# @bot.event
+# async def on_command_error(ctx, error):
+#     if isinstance(error, commands.CommandNotFound):
+#         pass
 if __name__ == '__main__':
     reddit = praw.Reddit(SITE)
     subreddit = reddit.subreddit(SUB)
